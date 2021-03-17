3fe568cce3d38e11729be9aa2997039aa135ca84
Michael Tew <mike.tew@gmail.com>
2021-03-16 22:05:25 -0700

    Updated layout.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# TODO
+# Change colors depending on usage percentages.
+
 from tkinter import Tk, Label
 import psutil
 
@@ -6,20 +9,27 @@ def get_resource_usage():
     """Get and display CPU and RAM usage every second."""
 
     cpu_usage = psutil.cpu_percent()
-    cpu_label.config(text=f"CPU\n{cpu_usage}")
+    cpu_load_label.config(text=f"{cpu_usage}%")
     memory_usage = psutil.virtual_memory()[2]
-    memory_label.config(text=f"RAM\n{memory_usage}%")
+    memory_load_label.config(text=f"{memory_usage}%")
 
     cpu_label.after(1000, get_resource_usage)
 
 root = Tk()
 root.geometry("+700+300")
+root.title("Resource Usage")
 
-cpu_label = Label(root, text="CPU Load", font=("Helvetica 20"))
-cpu_label.grid(row=0, column=0, padx=30)
+cpu_label = Label(root, text="CPU", font=("Helvetica 15"))
+cpu_label.grid(row=0, column=0, padx=20, pady=[10, 0])
 
-memory_label = Label(root, text="Memory", font=("Helvetica 20"))
-memory_label.grid(row=0, column=1, padx=30)
+cpu_load_label = Label(root, text="0.0%", font=("Helvetica 13"))
+cpu_load_label.grid(row=1, column=0, pady=[0, 10])
+
+memory_label = Label(root, text="RAM", font=("Helvetica 15"))
+memory_label.grid(row=0, column=1, padx=20, pady=[10, 0])
+
+memory_load_label = Label(root, text="0.0%", font=("Helvetica 13"))
+memory_load_label.grid(row=1, column=1, pady=[0, 10])
 
 get_resource_usage()
 
